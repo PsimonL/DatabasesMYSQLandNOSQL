16abf9607d1e927cd7d7b9d849ccf916dfcce38b
Szymon Rogowski <szymon0r0official@gmail.com>
2022-12-31 00:24:21 +0100

    Adding Functions to resume data

--- School Library Management System/22_functions__procedures_example_Data_Sheet.sql
+++ School Library Management System/22_functions__procedures_example_Data_Sheet.sql
@@ -3,3 +3,19 @@
 -- Example data sheets - FUNCTIONS / PROCEDURES
 -- ###############################################################################################################################
 -- ###############################################################################################################################
+
+-- Number of all copies in library.
+DROP FUNCTION IF EXISTS CurrentQuantityOfLibrary();
+CREATE FUNCTION CurrentQuantityOfLibrary()
+RETURNS int
+LANGUAGE plpgsql
+AS $$
+    DECLARE
+        no_books int;
+    BEGIN
+       SELECT sum(quantity_books.quantity) INTO no_books FROM quantity_books WHERE quantity > 0;
+       RETURN no_books;
+    END;
+$$;
+SELECT CurrentQuantityOfLibrary() AS NO_COPIES_in_the_entire_library;
+
